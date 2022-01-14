a6fa685e73b0718013d84be628eb838eb7836ddb
Elis <byhovtsov@gmail.com>
2022-01-14 09:34:45 +0300

    2_02_h2_init

--- src/main/java/ru/javaops/bootjava/repository/UserRepository.java
+++ src/main/java/ru/javaops/bootjava/repository/UserRepository.java
@@ -3,5 +3,6 @@ package ru.javaops.bootjava.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.javaops.bootjava.model.User;
 
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
